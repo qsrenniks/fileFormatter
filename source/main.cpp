ffaa5304f2e58213b6cef2793a0f16d0b08745c4
qsrenniks <q.skinner@digipen.edu>
2020-09-23 14:40:39 -0700

    added file parsing to make file formatting better

--- source/main.cpp
+++ source/main.cpp
@@ -1,109 +1,180 @@
+#include "pch.h"
 #include <iostream>
 #include <filesystem>
 #include <fstream>
 #include <ctime>
 #include <sstream>
+#include <vector>
+#include <algorithm>
+
+struct FileType
+{
+  std::string m_extension;
+  std::vector<std::string> m_lines;
+};
+
+struct FileData
+{
+  std::string m_filenameWithExtension;
+  std::string m_filenameWithoutExtension;
+};
+
+static FileData m_activeData;
+
+const std::string resolveVariable(const std::string& variableName)
+{
+  if (variableName == "$FILE_NAME$")
+  {
+    return m_activeData.m_filenameWithExtension;
+  }
+  else if (variableName == "$DATE$")
+  {
+    std::stringstream stream;
+    time_t t = time(nullptr);
+    tm* timePtr = localtime(&t);
+    stream << timePtr->tm_year + 1900 << "/" << timePtr->tm_mon << "/" << timePtr->tm_mday;
+    return stream.str();
+  }
+  else if (variableName == "$FILE_UPPER$")
+  {
+    std::string name = m_activeData.m_filenameWithoutExtension;
+    std::transform(name.begin(), name.end(), name.begin(), [](char a) -> char
+    {
+      return std::toupper(a);
+    });
+    return name;
+  }
+  else
+  {
+    std::cout << "unknown variable: " << variableName << std::endl;
+  }
+  return variableName;
+}
+
+static std::vector<FileType> m_fileTypes;
 
 static bool isValidFilePath(const std::filesystem::path& filepath)
 {
-	std::string exten = filepath.extension().string();
-	if(exten == ".cpp" || exten == ".h")
-	{
-		return true;
-	}
-	else return false;
+  std::string exten = filepath.extension().string();
+  if (exten == ".cpp" || exten == ".h")
+  {
+    return true;
+  }
+  else return false;
 }
 
 static bool isSourceFile(const std::filesystem::path& filepath)
 {
-	//std::cout << "filestem: " << filepath.extension().string() << std::endl;
-	if(filepath.extension().string() == ".cpp")
-	{
-		return true;
-	}
-	else return false;
+  //std::cout << "filestem: " << filepath.extension().string() << std::endl;
+  if (filepath.extension().string() == ".cpp")
+  {
+    return true;
+  }
+  else return false;
 }
 
-static bool isHeaderFile(const std::filesystem::path& filepath)
+static void createFileTypeFromJsonObject(FileType& type, const json& object)
 {
-	return !isSourceFile(filepath);
+  type.m_extension = object["extension"].get<std::string>();
+
+  for (const auto& lineObject : object["style"])
+  {
+    std::string line = lineObject.get<std::string>();
+    type.m_lines.push_back(line);
+  }
 }
 
-static std::string buildFileHeaderComment(const std::filesystem::path& filepath)
+static void parseFormatFile(std::vector<FileType>& files, std::filesystem::path& formatPath)
 {
-	//this should later become a command arg
-	
-	time_t t = time(nullptr);
-	tm* timePtr = localtime(&t);
-	
-	std::stringstream headerComment;
-	headerComment << "/*!" << std::endl;
-	headerComment << " * \\file " << filepath.filename().string() << std::endl;
-	headerComment << " * \\author Quinton Skinner" << std::endl;
-	headerComment << " * \\par Email: qsrenniks\\@gmail.com" << std::endl;
-	headerComment << " * \\par " << timePtr->tm_year + 1900 << "/" << timePtr->tm_mon << "/" << timePtr->tm_mday << std::endl;
-	headerComment << " */" << std::endl;
-	return headerComment.str();
+  std::ifstream file;
+  file.open(formatPath);
+  if (file.is_open())
+  {
+    json fileData;
+    //     fileData << file;
+    file >> fileData;
+
+    for (const auto& object : fileData["fileTypes"])
+    {
+      FileType type;
+      createFileTypeFromJsonObject(type, object);
+      files.push_back(type);
+    }
+  }
 }
 
-static std::string buildHeaderGuard(const std::filesystem::path& filepath)
+static const FileType* matchFileTypeToExtension(const std::filesystem::path& file)
 {
-	std::string headerTag = filepath.stem().string();
-	for(unsigned i = 0; i < headerTag.size(); ++i)
-	{
-		headerTag[i] = std::toupper(headerTag[i]);
-	}
-	
-	std::stringstream headerGuard;
-	headerGuard << "#ifndef " << headerTag << "_H" << std::endl;
-	headerGuard << "#define " << headerTag << "_H" << std::endl << std::endl;
-	headerGuard << "#endif " << std::endl;
-	return headerGuard.str();
+  std::string extension = file.extension().string();
+  for (const auto& type : m_fileTypes)
+  {
+    if (type.m_extension == extension)
+    {
+      return &type;
+    }
+  }
+  return nullptr;
+}
+
+static const std::string formatLine(const std::string& line)
+{
+  std::string formattedLine = line;
+  auto startOfVar = line.find('$');
+  auto endOfVar = line.find('$', startOfVar);
+  if (startOfVar != std::string::npos && endOfVar != std::string::npos)
+  {
+    const std::string varName = line.substr(startOfVar, startOfVar + endOfVar);
+    const std::string variable = resolveVariable(varName);
+    formattedLine.replace(startOfVar, endOfVar, variable);
+  }
+  return formattedLine;
+}
+
+static void formatFileWithFileType(const FileType& type, std::ofstream& file)
+{
+  for (const auto& line : type.m_lines)
+  {
+    const std::string formattedLine = formatLine(line);
+    file << formattedLine << std::endl;
+  }
 }
 
 int main(int argc, const char** argv)
 {
-	if(argc < 2)
-	{
-		std::cout << "============ USAGE ============" << std::endl;
-		std::cout << "ff.exe <FILENAME>" << std::endl;
-		std::cout << "===============================" << std::endl;
-	}
-	
-	std::filesystem::path currentPath = std::filesystem::current_path();
-	std::filesystem::path filePath = currentPath / argv[1];
-	
-	if(!isValidFilePath(filePath))
-	{
-		std::cout << "file extension is not supported, only .cpp and .h are supported" << std::endl;
-		return 0;
-	}
-	
-	if(!std::filesystem::exists(filePath)) // program ditches on existing files
-	{
-		std::ofstream file;
-		file.open(filePath);
-		
-		if(file)
-		{
-			std::stringstream fileData;
-			fileData << buildFileHeaderComment(filePath);
-			fileData << std::endl;
-			
-			if (!isSourceFile(filePath))
-			{
-				fileData << buildHeaderGuard(filePath);
-			}
-			file << fileData.str();
-		}
-		
-		file.close();
-	}
-	else
-	{
-		std::cout << "file already exists" << std::endl;
-		return 0;
-	}
-	
-	return 0;
+  if (argc < 3)
+  {
+    std::cout << "============ USAGE ===========" << std::endl;
+    std::cout << "ff.exe <FILEFORMAT> <FILENAME>" << std::endl;
+    std::cout << "==============================" << std::endl;
+  }
+
+  std::filesystem::path currentPath = std::filesystem::current_path();
+  std::filesystem::path formatPath = currentPath / argv[1];
+  std::filesystem::path filePath = currentPath / argv[2];
+
+  parseFormatFile(m_fileTypes, formatPath);
+
+  if (!std::filesystem::exists(filePath)) // program ditches on existing files
+  {
+    std::ofstream file;
+    file.open(filePath);
+
+    if (file)
+    {
+      const FileType* type = matchFileTypeToExtension(filePath);
+      if (type)
+      {
+        formatFileWithFileType(*type, file);
+      }
+    }
+
+    file.close();
+  }
+  else
+  {
+    std::cout << "file already exists" << std::endl;
+    return 0;
+  }
+
+  return 0;
 }
---
+++ source/pch.h
@@ -0,0 +1,13 @@
+/*!
+ * \file pch.h
+ * \author Quinton Skinner
+ * \par Email: qsrenniks\@gmail.com
+ * \par 2020/8/23
+ */
+
+#ifndef PCH_H
+#define PCH_H
+
+#include <json.hpp>
+
+#endif 
